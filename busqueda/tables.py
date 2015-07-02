b898343f360d1e39caa37063f5528c295ac53224
cgalceran <cgalceran@Carlos-MacBook-Pro.local>
2015-07-02 16:11:36 -0300

    deleted django_tables2 file from system

--- busqueda/tables.py
+++
@@ -1,9 +0,0 @@
-import django_tables2 as tables
-from models import Expediente
-
-class ExpedienteTable(tables.Table):
-    class Meta:
-        model = Expediente
-        #fields = ('distribuidora','name','rendicion','total','nota')
-        # add class="paleblue" to <table> tag
-        attrs = {"class": "paleblue"}
